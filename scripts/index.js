0963cebebb6645a1f04021c77b007350af8ea5f5
Julia S <julia297711@yandex.ru>
2024-07-20 19:22:34 +0700

    11

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placeList = document.querySelector(".places__list");
+
+function createCards(item) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const placesItem = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const cardImage = placesItem.querySelector(".card__image");
+  const cardTitle = placesItem.querySelector(".card__title");
+
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
+  placesItem.querySelector('.card__delete-button').addEventListener('click', clickRemoveCard);
+  return placesItem;
+}
+initialCards.forEach((item) => {
+  // перебор массива
+  placeList.append(createCards(item));
+});
+
+  // Функция для удлаения карточек, слушатель будет при создании карточки
+  function clickRemoveCard(event) {
+    event.target.closest(".places__item").remove();
+  }
